a66b541f0eb4944645ef0bda4fdcbc1b934413f9
Sándor Nagy <privsala@gmail.com>
2017-02-11 22:46:05 +0100

    Added profile update POST, extended ivy and improved readme

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # Storage Service for the Final Project at IntroSDE
 
-description
+## API Endpoints
+
+### /StorageService/UserProfile?userId={userId} [GET, POST]
+
+Calls the REST API endpoint of the LocalDatabase [PROTOCOL] /sdelab/person/{userId} and returns the result
+Details of the call can be found at the Local Database description
+
+### /StorageService/WeatherInformation?city={cityId} [GET]
+
+Calls the SOAP API endpoint of the WeatherAdapter [PROTOCOL] getWeatherInformationByCity({cityId}) and returns the result
+Details of the call can be found at the Weather Adapter description
+
+### /StorageService/addUpdateStepCount [PUT]
+
+Calls the REST API endpoint of the LocalDatabase [PROTOCOL] /sdelab/steps/{userId} and returns the result
+Details of the call can be found at the Local Database description
+
+## Copyright
+
+&copy; Sándor Tibor Nagy as the final project for Introduction to Service Design and Engineering course 2016/2017 at [UNITN](http://www.unitn.it/)
--- src/introsde/rest/StorageService.java
+++ src/introsde/rest/StorageService.java
@@ -4,12 +4,16 @@ import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriBuilder;
@@ -17,6 +21,7 @@ import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
 
 import org.glassfish.jersey.client.ClientConfig;
+import org.json.JSONObject;
 
 import introsde.ws.Weather;
 
@@ -31,13 +36,14 @@ public class StorageService {
 	@Path("UserProfile")
 	@Produces(MediaType.APPLICATION_JSON)
 	public String UserProfile(@QueryParam("userId") int userId) {
+		System.out.println("--> UserProfile called with userId = " + userId);
 		ClientConfig clientConfig = new ClientConfig();
         Client client = ClientBuilder.newClient(clientConfig);
         WebTarget service = client.target(getBaseURI());
         
-        System.out.println(userId);
-
         String user_data = service.path("person/" + userId).request().accept(MediaType.APPLICATION_JSON).get().readEntity(String.class);
+        System.out.println(user_data);
+        
 		return user_data;
 	}
 	
@@ -45,6 +51,7 @@ public class StorageService {
 	@Path("WeatherInformation")
 	@Produces(MediaType.TEXT_PLAIN)
 	public String WeatherInformation(@QueryParam("city") String city) throws MalformedURLException {
+		System.out.println("--> WeatherInformation called with city = " + city);
         /// Build stuff for SOAP
         URL url = new URL("https://final-weather-adapter.herokuapp.com/ws/weather?wsdl");
         // 1st argument service URI, refer to wsdl document above
@@ -53,10 +60,52 @@ public class StorageService {
         Service service = Service.create(url, qname);
         Weather hello = service.getPort(Weather.class);
         /// ---
-        System.out.println(city);
         String result = hello.getWeatherInformationByCity(city);
+        System.out.println(result);
 
         return result;
+	}
+	
+	@PUT
+    @Path("/addUpdateStepCount") // you can pass path params to a service
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+	public String addUpdateStep(String payload) {
+    	System.out.println("--> /addUpdateStepCount called with payload:");
+    	System.out.println(payload);
+    	
+    	// Get JSON from the payload
+    	JSONObject json_data = new JSONObject(payload);
+    	
+    	// Pass the data to the Storage Layer
+    	ClientConfig clientConfig = new ClientConfig();
+        Client client = ClientBuilder.newClient(clientConfig);
+        WebTarget service = client.target(getBaseURI());
+        
+        String response_text = service.path("steps").request().accept(MediaType.APPLICATION_JSON).put(Entity.json(json_data.toString())).readEntity(String.class);
+        
+		return response_text;
+	}
+	
+	@POST
+    @Path("/UserProfile") // you can pass path params to a service
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+	public String addUpdateProfile(@QueryParam("userId") int userId, String payload) {
+    	System.out.println("--> /addUpdateProfile called with userId = "+userId+" and payload:");
+    	System.out.println(payload);
+    	
+    	// Get JSON from the payload
+    	JSONObject json_data = new JSONObject(payload);
+    	
+    	// Pass the data to the Storage Layer
+    	ClientConfig clientConfig = new ClientConfig();
+        Client client = ClientBuilder.newClient(clientConfig);
+        WebTarget service = client.target(getBaseURI());
+        
+        String response_text = service.path("person/"+userId).request().accept(MediaType.APPLICATION_JSON).post(Entity.json(json_data.toString())).readEntity(String.class);
+        
+		return response_text;
 	}
 	
 	private static URI getBaseURI() {
